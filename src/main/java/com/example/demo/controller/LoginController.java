3888600ffdbb4b96018800d28be25638df857951
RINAYUKAWA\kami9 <kami9.pidamu@gmail.com>
2025-08-15 12:31:37 +0900

    LoginControllerにlocalhost:8080/loginにアクセスされたらloginForm.htmlを表示する処理を記述。

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,5 +1,21 @@
 package com.example.demo.controller;
 
-public class LoginController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.demo.service.LoginService;
+
+@Controller
+
+public class LoginController {
+	
+	@Autowired
+	private LoginService loginService;
+	
+	@GetMapping("/login")
+		public String getLogin(Model model) {
+		return "loginForm";
+	}
 }
