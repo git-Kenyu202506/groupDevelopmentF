951276d0bac990cebdb7c46e2233dfe28b973b01
RINAYUKAWA\kami9 <kami9.pidamu@gmail.com>
2025-09-07 18:21:01 +0900

    DeleteMapperインターフェース作成

---
+++ src/main/java/com/example/demo/mapper/DeleteMapper.java
@@ -0,0 +1,12 @@
+package com.example.demo.mapper;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface DeleteMapper {
+	
+	@Delete("DELETE FROM staff WHERE id = #{id}")
+	public void delete(int id);
+
+}
