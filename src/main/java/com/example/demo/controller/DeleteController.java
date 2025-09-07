0cdfe8a4f556609ea77398316b4272b239378151
RINAYUKAWA\kami9 <kami9.pidamu@gmail.com>
2025-09-07 18:21:54 +0900

    DeleteControllerクラス作成

---
+++ src/main/java/com/example/demo/controller/DeleteController.java
@@ -0,0 +1,66 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.service.DeleteService;
+
+@Controller
+
+@RequestMapping("/delete")
+public class DeleteController {
+	
+	@Autowired
+	private DeleteService deleteService;
+	
+	
+//	localhost:8080/delete/formにアクセス→削除入力画面を表示
+	@GetMapping("/form")
+	public String deleteForm() {
+		return "deleteForm";
+	}
+	
+//	localhost:8080/delete/checkにアクセス→削除確認画面を表示
+	@PostMapping("/check")
+	public String deleteCheck(
+			Model model,
+			@RequestParam("id")List<Integer>idList,
+			@RequestParam("url")String url){
+		model.addAttribute("check",idList);
+		model.addAttribute("back",url);
+		return "deleteCheck";
+	}
+	
+//	localhost:8080/delete/executionにアクセス→削除処理→削除完了画面
+	@PostMapping("/execution")
+	public String deleteExecution(
+			Model model,
+			@RequestParam("id")List<Integer>idList) {
+		deleteService.delete(idList);
+		model.addAttribute("comp","削除が完了しました");
+		return "deleteCompletion";
+	}
+	
+	
+//	localhost:8080/delete/backにアクセス→urlに応じたページに遷移[戻る処理]
+	@GetMapping("/back")
+	public String deleteBack(
+			Model model,
+			@RequestParam("url")String url){
+		
+				if(url.equals ("deleteForm")) {
+					return "deleteForm";
+				}else {
+					return "searchForm";
+				}
+			}
+	
+
+}
